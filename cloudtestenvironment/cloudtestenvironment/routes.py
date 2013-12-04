7b6ac08d3a0a54504b95c68cce7665f7994beb12
acthomas06@github.com <acthomas06@github.com>
2013-12-04 13:47:36 -0700

    updated

--- cloudtestenvironment/cloudtestenvironment/routes.py
+++ cloudtestenvironment/cloudtestenvironment/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template, redirect, url_for, request, session
 from forms import RegistrationForm, ContactForm, PaypalForm, CreditCardForm, OrderForm
-from cloudtestenvironment import app
 from models import db, Customer, Order, OrderItems
 from time import strftime
 
